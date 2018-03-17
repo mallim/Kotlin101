0fe13c191f80659f06a4366dd75d8f4bc990e1bc
IanLim <mallim.ink@gmail.com>
2018-03-17 18:42:18 +0800

    Finished kotlin101

--- src/Collections/ArrayList/ArrayList.kt
+++ src/Collections/ArrayList/ArrayList.kt
@@ -1,5 +1,7 @@
 package Collections.ArrayList
 
+import kotlin.test.assertEquals
+
 fun main(args: Array<String>) {
     val list = arrayListOf( 1, 2, 3 )
     list.forEach{ println( it )}
@@ -9,4 +11,32 @@ fun main(args: Array<String>) {
     list2.add( Pair( "Hillary", "Clinton" ) )
 
     list2.forEach{ println( "${it.first} - ${it.second} " ) }
+
+    //
+    // Got this from https://github.com/steelkiwi/collection_in_kotlin
+    //
+    // note that filter and dropWhile return a new instance like many other operators.
+    // That means originalList remains unchanged.
+    //
+    val originalList = listOf(1, 2, 3, 4, 5, 6)
+    assertEquals(listOf(2, 4, 6), originalList.filter { it % 2 == 0 })
+
+    val originalList2 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = originalList2.firstOrNull { it > 4 }
+    assertEquals(result, 5)
+
+    val originalList3 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result2 = originalList3.getOrElse(12) { 12 }
+    assertEquals(result2, 12)
+
+    val originalList4 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result3 = originalList4.dropWhile { it < 5 }
+    assertEquals(result3, listOf(5, 6, 7, 8, 9, 10))
+
+    val originalList5 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result4 = originalList5
+            .dropWhile { it < 5 }
+            .find { it < 7 }
+    assertEquals(result4, 5)
+
 }
---
+++ src/Functions/LocalClass/LocalClass.kt
@@ -0,0 +1,10 @@
+package Functions.LocalClass
+
+fun main(Args : Array<String>) {
+    class AirForce(val name : String)
+
+    val usa = AirForce("USA")
+    val india = AirForce("India")
+
+    println("${usa.name} and ${india.name}")
+}
